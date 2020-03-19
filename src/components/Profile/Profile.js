8647da2c025baf547ec35db8af7936e25dc321bc
rerueter <rerueter@gmail.com>
2020-03-18 17:09:12 -0700

    more work on user update

--- src/components/Profile/Profile.js
+++ src/components/Profile/Profile.js
@@ -1,13 +1,18 @@
 import React from "react";
 import axios from "axios";
 import { Segment, Header, Divider, Button } from "semantic-ui-react";
+import ProfileUpdate from "./ProfileUpdate/ProfileUpdate";
 
 class Profile extends React.Component {
   state = {
-    user: {}
+    user: {},
+    editing: false
   };
 
   componentDidMount = () => {
+    this.getUser();
+  };
+  getUser = () => {
     axios
       .get(`${process.env.REACT_APP_API}/users/${localStorage.getItem("uid")}`)
       .then(res => {
@@ -20,20 +25,36 @@ class Profile extends React.Component {
         console.log(err);
       });
   };
+  handleEdit = () => {
+    this.setState({
+      editing: !this.state.editing
+    });
+  };
   render() {
     return (
-      <Segment>
-        <Header> {`Welcome back, ${this.state.user.name}`}.</Header>
-        <Divider />
-        <h4>Email: {this.state.user.email}</h4>
-        <h4>Phone: {this.state.user.tel}</h4>
-        <h4>Medical Information: {this.state.user.emi}</h4>
-        <h4>Emergency Contact: {this.state.user.ecName}</h4>
-        <h4>Emergency Contact Phone: {this.state.user.ecTel}</h4>
-        <h4>Emergency Contact Relationship: {this.state.user.ecRel}</h4>
-        <Divider />
-        <Button>Edit</Button>
-      </Segment>
+      <>
+        {!this.state.editing && (
+          <Segment>
+            <Header> {`Welcome back, ${this.state.user.name}`}.</Header>
+            <Divider />
+            <h4>Email: {this.state.user.email}</h4>
+            <h4>Phone: {this.state.user.tel}</h4>
+            <h4>Medical Information: {this.state.user.emi}</h4>
+            <h4>Emergency Contact: {this.state.user.ecName}</h4>
+            <h4>Emergency Contact Phone: {this.state.user.ecTel}</h4>
+            <h4>Emergency Contact Relationship: {this.state.user.ecRel}</h4>
+            <Divider />
+            <Button onClick={this.handleEdit}>Edit</Button>
+          </Segment>
+        )}
+        {this.state.editing && (
+          <ProfileUpdate
+            user={this.state.user}
+            handleEdit={this.handleEdit}
+            getUser={this.getUser}
+          />
+        )}
+      </>
     );
   }
 }
--- src/components/Profile/ProfileUpdate/ProfileUpdate.js
+++ src/components/Profile/ProfileUpdate/ProfileUpdate.js
@@ -1,14 +1,89 @@
-import React from "react";
+import React, { useReducer } from "react";
 import { Divider, Form, Button, Segment } from "semantic-ui-react";
 import axios from "axios";
 
 class ProfileUpdate extends React.Component {
+  state = {
+    _id: this.props.user._id,
+    tel: this.props.user.tel,
+    emi: this.props.user.emi,
+    ecName: this.props.user.ecName,
+    ecTel: this.props.user.ecTel,
+    ecRel: this.props.user.ecRel,
+    avatar: this.props.user.avatar
+  };
+
+  handleChange = event => {
+    console.log(event.target.value);
+    this.setState({ [event.target.name]: event.target.value });
+  };
+
+  handleSubmit = event => {
+    event.persist();
+    axios
+      .put(`${process.env.REACT_APP_API}/users/${this.state._id}`, this.state)
+      .then(res => {
+        console.log(res);
+        this.props.handleEdit();
+        this.props.getUser();
+      })
+      .catch(err => console.log(err));
+  };
+
   render() {
+    const { tel, emi, ecName, ecTel, ecRel, avatar } = this.state;
     return (
       <Segment>
-        <Divider />
-        <p>Howdy</p>
-        <Divider />
+        <Form>
+          <Form.Input
+            type="text"
+            label="Phone"
+            name="tel"
+            value={`${tel}`}
+            onChange={this.handleChange}
+          />
+          <Form.TextArea
+            type="text"
+            label="Medical Information"
+            name="emi"
+            value={`${emi}`}
+            onChange={this.handleChange}
+          />
+          <Form.Input
+            type="text"
+            label="Emergency Contact Name"
+            name="ecName"
+            value={`${ecName}`}
+            onChange={this.handleChange}
+          />
+          <Form.Input
+            type="text"
+            label="Emergency Contact Phone"
+            name="ecTel"
+            value={`${ecTel}`}
+            onChange={this.handleChange}
+          />
+          <Form.Input
+            type="text"
+            label="Emergency Contact Relation"
+            name="ecRel"
+            value={`${ecRel}`}
+            onChange={this.handleChange}
+          />
+          <Form.Input
+            type="text"
+            label="Image"
+            name="avatar"
+            value={`${avatar}`}
+            onChange={this.handleChange}
+          />
+          <Button.Group>
+            <Form.Button onClick={this.handleSubmit}> Save Changes</Form.Button>
+            <Form.Button onClick={this.props.handleEdit}>
+              Cancel Edit
+            </Form.Button>
+          </Button.Group>
+        </Form>
       </Segment>
     );
   }
